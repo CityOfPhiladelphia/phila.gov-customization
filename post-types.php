571c4187c5181246e1328b6b78c986f348c70cbd
Karissa <k@karissademi.com>
2014-11-13 13:18:18 -0500

    "pages" are now "Department Pages"

--- post-types.php
+++ post-types.php
@@ -10,26 +10,27 @@
  * @package phila.gov-customization
  */
 
-if (!class_exists("PhilaGovCustomPostTypes")){
-    class PhilaGovCustomPostTypes {
-            function change_admin_post_label(){
-                global $menu;
-                global $submenu;
+if (!class_exists("PhilaGovCustomAdminLabels")){
+    class PhilaGovCustomAdminLabels {
+        function change_admin_post_label(){
+            global $menu;
+            global $submenu;
                 
-                $menu[5][0] = 'Information Page';
-                $submenu['edit.php'][5][0] = 'Information Page';
-                $submenu['edit.php'][10][0] = 'Add Information Page';
+            $menu[5][0] = 'Information Page';
+            $submenu['edit.php'][5][0] = 'Information Page';
+            $submenu['edit.php'][10][0] = 'Add Information Page';
                 
-                echo '';
+            echo '';
         }
         
         function change_admin_post_object(){
             global $wp_post_types;
+            //can't extract $lables in one go, so break it into 2 vars
             $get_post = $wp_post_types['post'];
             $lables = $get_post -> labels;
-            $lables -> name = 'Information';
+            $lables -> name = 'Information Page';
             $lables -> singular_name = 'Information';
-            $lables -> add_new = 'Add Information';
+            $lables -> add_new = 'Add Information Page';
             $lables -> add_new_item = 'Add Information Page';
             $lables -> edit_item = 'Edit Information Page';
             $lables -> new_item = 'Information';
@@ -39,19 +40,53 @@ if (!class_exists("PhilaGovCustomPostTypes")){
             $lables -> not_found_in_trash = 'No Information Page found in Trash';
             $lables -> all_items = 'All Information Pages';
             $lables -> menu_name = 'Information Page';
-            $lables -> name_admin_bar = 'Information';
-
+            $lables -> name_admin_bar = 'Information Page';
         }
-    }//end PhilaGovCustomTax
+        
+        function change_admin_page_label(){
+            global $menu;
+            global $submenu;
+                
+            $menu[20][0] = 'Department Page';
+            $submenu['edit.php?post_type=page'][5][0] = 'Department Page';
+            $submenu['edit.php?post_type=page'][10][0] = 'Add Department Page';
+                
+            echo '';
+        }
+        
+        function change_admin_page_object(){
+            global $wp_post_types;
+            //can't extract $lables in one go, so break it into 2 vars
+            $get_page = $wp_post_types['page'];
+            $lables = $get_page -> labels;
+            $lables -> name = 'Department Page';
+            $lables -> singular_name = 'Department';
+            $lables -> add_new = 'Add Department Page';
+            $lables -> add_new_item = 'Add Department Page';
+            $lables -> edit_item = 'Edit Department Page';
+            $lables -> new_item = 'Department';
+            $lables -> view_item = 'View Department Page';
+            $lables -> search_items = 'Search Department Pages';
+            $lables -> not_found = 'No Department Page Found';
+            $lables -> not_found_in_trash = 'No Department Page found in Trash';
+            $lables -> all_items = 'All Department Pages';
+            $lables -> menu_name = 'Department Page';
+            $lables -> name_admin_bar = 'Department Page';
+        }
+    }//end PhilaGovCustomAdminLables
 }
 
 //create instance of PhilaGovCustomTax
-if (class_exists("PhilaGovCustomPostTypes")){
-    $admin_menu_lables = new PhilaGovCustomPostTypes();
+if (class_exists("PhilaGovCustomAdminLabels")){
+    $admin_menu_lables = new PhilaGovCustomAdminLabels();
 }
 
-if (isset($phila_gov_tax)){
+if (isset($admin_menu_lables)){
     //WP actions
-    add_action( 'admin_menu', array($admin_menu_lables, 'change_admin_post_label'));
     add_action( 'init', array($admin_menu_lables, 'change_admin_post_object'));
+    add_action( 'admin_menu', array($admin_menu_lables, 'change_admin_post_label'));
+    
+    add_action( 'init', array($admin_menu_lables, 'change_admin_page_object'));
+    add_action( 'admin_menu', array($admin_menu_lables, 'change_admin_page_label'));
+    
 }
