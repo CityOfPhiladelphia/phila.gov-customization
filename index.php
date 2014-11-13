5edbd39bbfaef345387a2fc883642b07aeba2999
Karissa <k@karissademi.com>
2014-11-13 11:35:58 -0500

    "topics" custom taxonomy
    creation of:
    index.php
    taxonomy.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 	
+/**
+ * Plugin Name: Phila.gov Customization
+ * Plugin URI: https://github.com/CityOfPhiladelphia/phila.gov-customization
+ * Description Custom Wordpress functionality, custom post types, custom taxonomies, etc.
+ * Version: 1.0
+ * Author: Karissa Demi
+ * Author URI: http://karissademi.com 
+ * 
+ * @package phila.gov-customization
+ */
+
+$dir = plugin_dir_path( __FILE__ );
+
+require $dir.'/taxonomies.php';
---
+++ taxonomies.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Add custom taxonomies
+ *
+ * Additional custom taxonomies can be defined here
+ * http://codex.wordpress.org/Function_Reference/register_taxonomy
+ *
+ * @link https://github.com/CityOfPhiladelphia/phila.gov-customization
+ * 
+ * @package phila.gov-customization
+ */
+
+if (!class_exists("PhilaGovCustomTax")){
+    class PhilaGovCustomTax {
+            function add_custom_taxonomies() {
+                register_taxonomy('topics', 
+                    array(
+                        'post',
+                        'page'
+                    ), array(
+                        'hierarchical' => true,
+                        // This array of options controls the labels displayed in the WordPress Admin UI
+                        'labels' => array(
+                            'name' => _x( 'Topics', 'taxonomy general name'),
+                            'singular_name' => _x( 'Topic', 'taxonomy singular name'),
+                            'menu_name' =>     __('Topics'),
+                            'search_items' =>  __( 'Search Topics' ),
+                            'all_items' =>     __( 'All Topics' ),
+                            'edit_item' =>     __( 'Edit Topic' ),
+                            'update_item' =>   __( 'Update Topic' ),
+                            'add_new_item' =>  __( 'Add New Topic' ),
+                            'new_item_name' => __( 'New Topic Name' ),
+                            'menu_name' =>     __( 'Topics' ),
+                        ),
+                    'public' => true,
+                    'show_admin_column' => true,
+                    // Control the slugs used for this taxonomy
+                    'rewrite' => array(
+                      'slug' => 'topics', // This controls the base slug that will display before each term
+                      'with_front' => false, // Don't display the category base before "/locations/"
+                      'hierarchical' => true // This will allow URL's like "/locations/boston/cambridge/"
+                    ),
+              ));
+            }
+    }//end PhilaGovCustomTax
+}
+
+//create instance of PhilaGovCustomTax
+if (class_exists("PhilaGovCustomTax")){
+    $phila_gov_tax = new PhilaGovCustomTax();
+}
+
+if (isset($phila_gov_tax)){
+    //actions
+    add_action( 'init', array($phila_gov_tax, 'add_custom_taxonomies'), 0 );
+    //filters
+}
