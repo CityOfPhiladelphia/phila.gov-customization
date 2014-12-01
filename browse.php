336d3a7021d804198b717216a0e7531918937bfb
Karissa <k@karissademi.com>
2014-12-01 16:43:40 -0500

    added comments

--- browse.php
+++ browse.php
@@ -8,19 +8,16 @@
  * @package phila.gov-customization
  */
 
-//global $parent_topic;
-
 function currentURL(){
-    $pageURL = 'http';
-        $pageURL .= "://";
+    $page_URL = 'http';
+        $page_URL .= "://";
         if ($_SERVER["SERVER_PORT"] != "80") {
-            $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+            $page_URL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
          } else {
-            $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+            $page_URL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
          }
-    $theURL = explode('/', rtrim($pageURL, '/'));
-    //var_dump($theURL);
-    return $theURL;
+    $the_URL = explode('/', rtrim($page_URL, '/'));
+    return $the_URL;
 }
 
 function display_topic_list() {
@@ -30,6 +27,7 @@ function display_topic_list() {
     $parent_topic = '';
     if ($url_last_item === 'browse' ) {
         get_parent_topics();
+    //use array of URL length to determine if we are parent or child
     }elseif ($topic === 5) {
         $parent_topic === end($get_URL);
         get_children_topics();
@@ -46,25 +44,22 @@ function display_browse_breadcrumbs(){
     $second_to_last = prev($url);
     $topic = count($get_URL);
     if ($topic === 5) {
-        echo '<li><a href="/browse/' . $url_last_item. '">' .  $url_last_item . '</a></li>';
+        echo '<li><a href="/browse/' . $url_last_item. '">' .  replace_dashes($url_last_item) . '</a></li>';
     }elseif ($topic === 6){
-        echo '<li><a href="/browse/' . $second_to_last . '">' .  $second_to_last . '</a></li>';
-        echo '<li><a href="/browse/' . $second_to_last . '/' . $url_last_item . '">'.  $url_last_item . '</a></li>';
+        echo '<li><a href="/browse/' . $second_to_last . '">' .  replace_dashes($second_to_last) . '</a></li>';
+        echo '<li><a href="/browse/' . $second_to_last . '/' . $url_last_item . '">'.  replace_dashes($url_last_item) . '</a></li>';
     }
 }
+
+
 
 function display_filtered_pages() {
     $get_URL = currentURL();
     $url_last_item =  end($get_URL);
     $topic = count($get_URL);
     $parent_topic = '';
-    if ($url_last_item === 'browse' ) {
-        
-    }elseif ($topic === 5) {
-        
-    }elseif ($topic === 6){
-        get_template_part( 'content', 'finder' );
-                
+    if ($topic === 6){
+        get_template_part( 'content', 'finder' );         
           while ( have_posts() ) : the_post();
               get_template_part( 'content', 'list' );
           endwhile; 
@@ -86,7 +81,7 @@ function get_parent_topics(){
     echo '</ul></nav>';
 }   
 
-
+//Utility function, not currently in use
 function list_parent_topics(){
     $parent_terms = get_terms('topics', array('orderby' => 'slug', 'parent' => 0));
     foreach($parent_terms as $key => $parent_term) {
@@ -123,20 +118,47 @@ function active_children_topics(){
     end($url);
     $parent_term = prev($url);
     
-    echo '<h2 class="current-topic">' . $parent_term . '</h2>';
+    echo '<h2 class="current-topic">' . replace_dashes($parent_term) . '</h2>';
     
     $child_terms = get_terms('topics', array('orderby' => 'slug', 'search' => $parent_term));
     
     $current_term = get_term_by( 'slug', $parent_term, 'topics' );
+    
+    $args = array(
+        'child_of' => $current_term->term_id,
+        'taxonomy' => $current_term->taxonomy,
+        'hide_empty' => 0,
+        'hierarchical' => true,
+        'depth'  => 1,
+        'title_li' => ''
+        );
+     wp_list_categories( $args );
 
-        //then set the args for wp_list_categories
-         $args = array(
-            'child_of' => $current_term->term_id,
-            'taxonomy' => $current_term->taxonomy,
-            'hide_empty' => 0,
-            'hierarchical' => true,
-            'depth'  => 1,
-            'title_li' => ''
-            );
-         wp_list_categories( $args );
+}
+
+function replace_dashes($string) {
+    $string = str_replace("-", " ", $string);
+    $string = str_replace("and", "&", $string);
+    return $string;
+}
+
+//utility function, not currently in use
+function get_topics(){
+    $parent_terms = get_terms('topics', array('orderby' => 'slug', 'parent' => 0));
+        foreach($parent_terms as $key => $parent_term) {
+            
+            echo '<li><h3><a href="/browse/' . $parent_term->slug . '">' . $parent_term->name . '</h3>'; 
+            echo '<p>' . $parent_term->description . '</p></a></li>';
+            
+            $child_terms = get_terms('topics', array('orderby' => 'slug', 'parent' => $parent_term->term_id));
+    
+            if($child_terms) {
+                echo '<ul>';
+                    foreach($child_terms as $key => $child_term) {
+                        echo '<li><a href="/browse/'. $parent_term->slug . '/' . $child_term->slug . '">' . $child_term->name . ' (child)</a></li>';
+                        //echo $child_term->description; 
+                    }
+                echo '</ul>';
+            }
+    }
 }
