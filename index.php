28479f442c3f0b6cfe12515764fde10f9077cba9
Karissa <k@karissademi.com>
2014-12-10 16:37:26 -0500

    0.2.0

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: Phila.gov Customization
  * Plugin URI: https://github.com/CityOfPhiladelphia/phila.gov-customization
  * Description: Custom Wordpress functionality, custom post types, custom taxonomies, etc.
- * Version: 0.1.3
+ * Version: 0.2.0
  * Author: Karissa Demi
  * Author URI: http://karissademi.com 
  * 
