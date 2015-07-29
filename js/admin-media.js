aa94d07a03802f5723449a3751f7fa98fc09575d
Karissa Demi <k@karissademi.com>
2015-07-29 16:24:27 -0400

    More specific div selection

--- js/admin-media.js
+++ js/admin-media.js
@@ -1,6 +1,7 @@
 /* For all admins */
 jQuery(document).ready(function($){
   if( $('.misc-pub-attachment input[value*=".pdf"]').val() ) {
-    $('.post-type-attachment input').prop( 'disabled', true );
+    $('.post-type-attachment #categorydiv input').prop( 'disabled', true );
+    $('.post-type-attachment #publication_typediv input').prop( 'disabled', true );
   }
 });
