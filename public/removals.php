e515e365505f73d2d3d3de395a475252e8f84570
Karissa Demi <k@karissademi.com>
2015-08-06 08:56:47 -0400

    Remove wlwmanifest and rsd

--- public/removals.php
+++ public/removals.php
@@ -27,3 +27,7 @@ add_filter('wp_headers', 'remove_x_pingback');
 // Remove all feed links because they don't do anything right now
 remove_action('wp_head', 'feed_links', 2);
 remove_action('wp_head', 'feed_links_extra', 3);
+
+//Remove these because we don't need them
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'rsd_link');
