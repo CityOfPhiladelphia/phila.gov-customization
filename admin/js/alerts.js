46db18458cfe03a3403ac2ceb23233aada049d32
Karissa Demi <k@karissademi.com>
2015-11-09 14:02:03 -0500

    Show/hide 'other' title field

--- admin/js/alerts.js
+++ admin/js/alerts.js
@@ -1,18 +1,20 @@
 /*
 * Admin Alerts Custom Js
 *
-* Show/hide other icon box on a
-*
 */
 
 jQuery(document).ready(function($) {
+  /*show/hide metaboxes if alert type is "other" */
   $('.other-icon').hide();
+  $('.type-other').hide();
 
   $('#phila_type').change(function(){
     if($('#phila_type').val() == 'Other') {
       $('.other-icon').show();
+      $('.type-other').show();
     } else {
       $('.other-icon').hide();
+      $('.type-other').hide();
     }
   });
 });
