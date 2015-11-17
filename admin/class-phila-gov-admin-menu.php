03b95496b4e4e4a8dd4c36fcf80026baa7e5fdfa
Karissa Demi <k@karissademi.com>
2015-11-16 22:07:33 -0500

    Information Pages get category

--- admin/class-phila-gov-admin-menu.php
+++ admin/class-phila-gov-admin-menu.php
@@ -21,7 +21,9 @@ class PhilaGovAdminMenu {
     add_action( 'admin_menu', array($this, 'change_admin_information_page_label' ) );
 
     add_filter( 'custom_menu_order', array($this, 'admin_menu_order' ) );
+
     add_filter( 'menu_order', array($this, 'admin_menu_order' ) );
+
 
  }
 
@@ -54,30 +56,7 @@ class PhilaGovAdminMenu {
     global $submenu;
     //Rename Pages
     $menu[20][0] = 'Information Page';
-  }
 
-  function change_admin_page_object(){
-
-      global $wp_post_types;
-      //can't extract $labels in one go, so break it into 2 vars
-      $get_page = $wp_post_types['page'];
-      $labels = $get_page -> labels;
-      $labels -> name = 'Information Page';
-      $labels -> singular_name = 'Information Page';
-      $labels -> add_new = 'Add Information Page';
-      $labels -> add_new_item = 'Add Information Page';
-      $labels -> edit_item = 'Edit Information Page';
-      $labels -> new_item = 'Add Information Page';
-      $labels -> view_item = 'View Information Page';
-      $labels -> search_items = 'Search Information Pages';
-      $labels -> not_found = 'No Information Page Found';
-      $labels -> not_found_in_trash = 'No Information Page found in Trash';
-      $labels -> all_items = 'All Information Pages';
-      $labels -> menu_name = 'Information Page';
-      $labels -> name_admin_bar = 'Information Page';
-
-      //also, register cats
-      register_taxonomy_for_object_type('category', 'page');
-
+    register_taxonomy_for_object_type('category', 'page');
   }
 }
